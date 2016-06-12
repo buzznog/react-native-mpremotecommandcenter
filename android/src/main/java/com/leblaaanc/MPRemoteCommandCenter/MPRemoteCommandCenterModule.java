e0e7e09a3fc564f41e93c61316b041f158a7fc56
Ankit Patial <ankit@pinkhandtech.com>
2016-06-12 13:55:29 +0530

    MediaSession callback and, mediaPlayer seekTo addition

--- android/src/main/java/com/leblaaanc/MPRemoteCommandCenter/MPRemoteCommandCenterModule.java
+++ android/src/main/java/com/leblaaanc/MPRemoteCommandCenter/MPRemoteCommandCenterModule.java
@@ -1,46 +1,97 @@
 package com.leblaaanc.MPRemoteCommandCenter;
 
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.facebook.react.bridge.*;
 
 import android.util.Log;
 import java.util.Map;
+import android.support.annotation.Nullable;
+import android.media.MediaPlayer;
+import android.media.session.MediaSession;
+
 
 public class MPRemoteCommandCenterModule extends ReactContextBaseJavaModule {
 
-    ReactApplicationContext reactContext;
+    ReactApplicationContext _reactContext;
 
-    public MPRemoteCommandCenterModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
+    private MediaPlayer   _mediaPlayer;
+    private MediaSession  _mediaSession;
+    private final String  NAME = "MPRemoteCommandCenter";
+
+  public MPRemoteCommandCenterModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    _reactContext = reactContext;
+    _mediaSession       = new MediaSession(_reactContext, NAME);
+  }
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  @ReactMethod
+  public void setNowPlayingInfo(final ReadableMap info) {
+    if (info != null) {
+      String artist               = info.getString("artist");
+      String albumTitle           = info.getString("albumTitle");
+      String albumArtist          = info.getString("albumArtist");
+      String title                = info.getString("title");
+      String artworkURL           = info.getString("artworkURL");
+      String duration             = info.getString("duration");
+      String elapsedPlaybackTime  = info.getString("elapsedPlaybackTime");
+
+      Log.d(NAME, "setNowPlayingInfo called");
     }
 
-    @Override
-    public String getName() {
-        return "MPRemoteCommandCenter";
+  }
+
+  @ReactMethod
+  public void setElapsedPlaybackTime(Integer elapsedPlaybackTime)
+  {
+    if (_mediaPlayer != null) {
+        _mediaPlayer = new MediaPlayer();
     }
+    _mediaPlayer.seekTo(elapsedPlaybackTime);
+    Log.d(NAME, "setElapsedPlaybackTime(" + elapsedPlaybackTime + ")");
+  }
 
+  private void registerRemoteControlEvents() {
 
+    _mediaSession.setCallback(new MediaSession.Callback() {
+         @Override
+         public void onPlay() {
+            super.onPlay();
+            sendEvent("play");
+            Log.d(NAME, "onPlay");
+         }
 
-    @ReactMethod
-    public void setNowPlayingInfo(final ReadableMap info) {
-        if (info != null) {
-            String artist               = info.getString("artist");
-            String albumTitle           = info.getString("albumTitle");
-            String albumArtist          = info.getString("albumArtist");
-            String title                = info.getString("title");
-            String artworkURL           = info.getString("artworkURL");
-            String duration             = info.getString("duration");
-            String elapsedPlaybackTime  = info.getString("elapsedPlaybackTime");
+         @Override
+         public void onPause() {
+            super.onPause();
+            sendEvent("pause");
+            Log.d(NAME, "onPause");
+         }
 
-            System.out.println("Got detail " + artist);
-        }
+         @Override
+         public void onSkipToNext() {
+            super.onSkipToNext();
+            sendEvent("nextTrack");
+            Log.d(NAME, "onSkipToNext");
+         }
 
-    }
+         @Override
+         public void onSkipToPrevious() {
+            super.onSkipToPrevious();
+            sendEvent("previousTrack");
+            Log.d(NAME, "onSkipToPrevious");
+         }
+    });
+  }
 
-    @ReactMethod
-    public void setElapsedPlaybackTime(Integer elapsedPlaybackTime)
-    {
-        System.out.println("Elasped time " + elapsedPlaybackTime);
-    }
+  private void sendEvent(String eventName) {
+      _reactContext
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit("RNMPRemoteCommandCenterEvent", eventName);
+  }
 
 }
