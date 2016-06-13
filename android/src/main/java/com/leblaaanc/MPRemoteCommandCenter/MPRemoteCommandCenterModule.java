3e61531002c440a5426fd0236b611ef08ce49a9f
Ankit Patial <ankit@pinkhandtech.com>
2016-06-13 11:23:36 +0530

    changes

--- android/src/main/java/com/leblaaanc/MPRemoteCommandCenter/MPRemoteCommandCenterModule.java
+++ android/src/main/java/com/leblaaanc/MPRemoteCommandCenter/MPRemoteCommandCenterModule.java
@@ -1,36 +1,97 @@
 package com.leblaaanc.MPRemoteCommandCenter;
 
-import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import android.util.Log;
+import java.util.Map;
+import android.support.annotation.Nullable;
+import android.media.MediaPlayer;
+import android.media.session.MediaSession;
 
-public class MPRemoteCommandCenterPackage implements ReactPackage {
 
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
+public class MPRemoteCommandCenterModule extends ReactContextBaseJavaModule {
 
-        modules.add(new MPRemoteCommandCenterModule(reactContext));
+    ReactApplicationContext _reactContext;
 
-        return modules;
+    private MediaPlayer   _mediaPlayer;
+    private MediaSession  _mediaSession;
+    private final String  NAME = "RNMPRemoteCommandCenterManager";
+
+  public MPRemoteCommandCenterModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    _reactContext = reactContext;
+    _mediaSession       = new MediaSession(_reactContext, NAME);
+  }
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  @ReactMethod
+  public void setNowPlayingInfo(final ReadableMap info) {
+    if (info != null) {
+      String artist               = info.getString("artist");
+      String albumTitle           = info.getString("albumTitle");
+      String albumArtist          = info.getString("albumArtist");
+      String title                = info.getString("title");
+      String artworkURL           = info.getString("artworkURL");
+      String duration             = info.getString("duration");
+      String elapsedPlaybackTime  = info.getString("elapsedPlaybackTime");
+
+      Log.d(NAME, "setNowPlayingInfo called");
     }
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
+  }
 
-    @Override
-    public List<ViewManager> createViewManagers(
-            ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+  @ReactMethod
+  public void setElapsedPlaybackTime(Integer elapsedPlaybackTime)
+  {
+    if (_mediaPlayer != null) {
+        _mediaPlayer = new MediaPlayer();
     }
+    _mediaPlayer.seekTo(elapsedPlaybackTime);
+    Log.d(NAME, "setElapsedPlaybackTime(" + elapsedPlaybackTime + ")");
+  }
 
+  private void registerRemoteControlEvents() {
+
+    _mediaSession.setCallback(new MediaSession.Callback() {
+         @Override
+         public void onPlay() {
+            super.onPlay();
+            sendEvent("play");
+            Log.d(NAME, "onPlay");
+         }
+
+         @Override
+         public void onPause() {
+            super.onPause();
+            sendEvent("pause");
+            Log.d(NAME, "onPause");
+         }
+
+         @Override
+         public void onSkipToNext() {
+            super.onSkipToNext();
+            sendEvent("nextTrack");
+            Log.d(NAME, "onSkipToNext");
+         }
+
+         @Override
+         public void onSkipToPrevious() {
+            super.onSkipToPrevious();
+            sendEvent("previousTrack");
+            Log.d(NAME, "onSkipToPrevious");
+         }
+    });
+  }
+
+  private void sendEvent(String eventName) {
+      _reactContext
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit("RNMPRemoteCommandCenterEvent", eventName);
+  }
 
 }
