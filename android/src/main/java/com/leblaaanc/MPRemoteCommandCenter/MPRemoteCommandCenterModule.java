cb9b5d23624169f149c4131f937d6118fa259c02
Ankit Patial <ankit@pinkhandtech.com>
2016-06-28 18:28:11 +0530

    MediaSessionCompat addition

--- android/src/main/java/com/leblaaanc/MPRemoteCommandCenter/MPRemoteCommandCenterModule.java
+++ android/src/main/java/com/leblaaanc/MPRemoteCommandCenter/MPRemoteCommandCenterModule.java
@@ -9,7 +9,10 @@ import java.util.List;
 import android.content.Context;
 import android.util.Log;
 import android.net.Uri;
+
 import android.os.PowerManager;
+import android.os.Looper;
+import android.os.Handler;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -21,7 +24,6 @@ import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.NotificationCompat;
 
-// ref: http://stackoverflow.com/questions/26569044/how-can-i-create-a-mediacontroller-instance-from-mediasessionmanager
 public class MPRemoteCommandCenterModule extends ReactContextBaseJavaModule {
 
   ReactApplicationContext _reactContext;
@@ -32,11 +34,17 @@ public class MPRemoteCommandCenterModule extends ReactContextBaseJavaModule {
   AudioManager mAudioManager;
   MediaSessionCompat mSessionCompat;
   final String  NAME = "RNMPRemoteCommandCenterManager";
+  final String  TAG = "RNMPRCCM";
 
   public MPRemoteCommandCenterModule(ReactApplicationContext reactContext) {
     super(reactContext);
     _reactContext = reactContext;
-    init(reactContext);
+    new Handler(Looper.getMainLooper()).post(new Runnable() {
+         @Override
+         public void run() {
+             init();
+         }
+    });
   }
 
   @Override
@@ -47,42 +55,51 @@ public class MPRemoteCommandCenterModule extends ReactContextBaseJavaModule {
   @ReactMethod
   public void setNowPlayingInfo(final ReadableMap info) {
     if (info != null) {
-      String artist = info.getString("artist");
-      String albumTitle = info.getString("albumTitle");
-      String albumArtist = info.getString("albumArtist");
-      String title = info.getString("title");
-      String artworkURL = info.getString("artworkURL");
-      String duration = info.getString("duration");
-      String elapsedPlaybackTime = info.getString("elapsedPlaybackTime");
-      mSessionCompat.setMetadata(new MediaMetadataCompat.Builder()
-             .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, artist)
-             .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ARTIST, albumArtist)
-             .putString(MediaMetadataCompat.METADATA_KEY_TITLE, title)
-             .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, albumTitle)
-             .putString(MediaMetadataCompat.METADATA_KEY_ART_URI, artworkURL)
-             .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI, artworkURL)
-             .putLong(MediaMetadataCompat.METADATA_KEY_DURATION, Long.parseLong(duration))
-             .build());
+        try {
+            Log.d(TAG, "==> called setNowPlayingInfo()");
+
+              String artist = info.getString("artist");
+              String albumTitle = info.getString("albumTitle");
+              String albumArtist = info.getString("albumArtist");
+              String title = info.getString("title");
+              String artworkURL = info.getString("artworkURL");
+              Integer duration = info.getInt("duration");
+              Integer elapsedPlaybackTime = info.getInt("elapsedPlaybackTime");
+
+              mSessionCompat.setMetadata(new MediaMetadataCompat.Builder()
+                     .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, artist)
+                     .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ARTIST, albumArtist)
+                     .putString(MediaMetadataCompat.METADATA_KEY_TITLE, title)
+                     .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, albumTitle)
+                     .putString(MediaMetadataCompat.METADATA_KEY_ART_URI, artworkURL)
+                     .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI, artworkURL)
+                     .putLong(MediaMetadataCompat.METADATA_KEY_DURATION, duration)
+                     .build());
 
 
-        if (isMediaPlaying()) {
-          NotificationCompat.Builder builder = new NotificationCompat.Builder(_reactContext);
-          NotificationCompat.MediaStyle style = new NotificationCompat.MediaStyle();
-          style.setMediaSession(mSessionCompat.getSessionToken());
-          style.setShowCancelButton(true);
-          builder.setAutoCancel(false);
-          builder.setContentTitle(title);
-          builder.setStyle(style);
-          builder.setOngoing(true);
+                if (isMediaPlaying()) {
+                  NotificationCompat.Builder builder = new NotificationCompat.Builder(_reactContext);
+                  NotificationCompat.MediaStyle style = new NotificationCompat.MediaStyle();
 
-          //builder.addAction(generateAction(R.drawable.ic_previous, MediaPlayerService.ACTION_PREV, MediaPlayerService.ACTION_PREV));
-          //builder.addAction(generateAction(R.drawable.ic_previous, MediaPlayerService.ACTION_PLAY, MediaPlayerService.ACTION_PLAY));
-          //builder.addAction(generateAction(R.drawable.ic_previous, MediaPlayerService.ACTION_NEXT, MediaPlayerService.ACTION_NEXT));
+                  style.setMediaSession(mSessionCompat.getSessionToken());
+                  style.setShowCancelButton(true);
 
-          //builder.setLargeIcon();
-          //builder.setSmallIcon(R.drawable.logo);
-          NotificationManagerCompat mNotificationManager = NotificationManagerCompat.from(_reactContext);
-          mNotificationManager.notify(200, builder.build());
+                  builder.setAutoCancel(false);
+                  builder.setContentTitle(title);
+                  builder.setStyle(style);
+                  builder.setOngoing(true);
+
+                  //builder.addAction(generateAction(R.drawable.ic_previous, MediaPlayerService.ACTION_PREV, MediaPlayerService.ACTION_PREV));
+                  //builder.addAction(generateAction(R.drawable.ic_previous, MediaPlayerService.ACTION_PLAY, MediaPlayerService.ACTION_PLAY));
+                  //builder.addAction(generateAction(R.drawable.ic_previous, MediaPlayerService.ACTION_NEXT, MediaPlayerService.ACTION_NEXT));
+                  //builder.setLargeIcon();
+                  //builder.setSmallIcon(R.drawable.logo);
+
+                  NotificationManagerCompat mNotificationManager = NotificationManagerCompat.from(_reactContext);
+                  mNotificationManager.notify(200, builder.build());
+                }
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
   }
@@ -94,45 +111,28 @@ public class MPRemoteCommandCenterModule extends ReactContextBaseJavaModule {
     if (_mediaPlayer != null) {
         _mediaPlayer = new MediaPlayer();
     }
-    //_mediaPlayer.seekTo(elapsedPlaybackTime);
-    Log.d(NAME, "setElapsedPlaybackTime(" + elapsedPlaybackTime + ")");
+    _mediaPlayer.seekTo(elapsedPlaybackTime);
+    Log.d(TAG, "==> setElapsedPlaybackTime(" + elapsedPlaybackTime + ")");
   }
 
-  private void sendEvent(String eventName) {
-    _reactContext
-      .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-      .emit("RNMPRemoteCommandCenterEvent", eventName);
-  }
 
   synchronized public boolean isMediaPlaying() {
     return _mediaPlayer != null && _mediaPlayer.isPlaying();
   }
 
-  // should be first time
-  synchronized private void init(ReactApplicationContext context) {
-      //create player
-      if (_mediaPlayer != null) {
-        return;
-      }
+  public void init() {
+       mSessionCompat = new MediaSessionCompat(_reactContext, NAME, null, null);
+       mSessionCompat.setActive(true);
+       mSessionCompat.setCallback(new MediaSessionCompatCallback());
+       mSessionCompat.setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS | MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+       mSessionCompat.setActive(true);
 
-      //mSessionCompat = new MediaSessionCompat(_reactContext, new MediaSessionImpl(NAME));
-      // mSessionCompat.setActive(true);
-      // mSessionCompat.setCallback(new MediaSessionCompatCallback());
-      // mSessionCompat.setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS | MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
-
-      _mediaPlayer = new MediaPlayer();
-      _mediaPlayer.setWakeMode(_reactContext, PowerManager.PARTIAL_WAKE_LOCK);
-      _mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-      //set listeners
-      //_mediaPlayer.setOnPreparedListener(this);
-      //_mediaPlayer.setOnCompletionListener(this);
-      //_mediaPlayer.setScreenOnWhilePlaying(true);
-      //_mediaPlayer.setOnErrorListener(this);
-      //_mediaPlayer.setOnBufferingUpdateListener(this);
-  }
+       _mediaPlayer = new MediaPlayer();
+       _mediaPlayer.setWakeMode(_reactContext, PowerManager.PARTIAL_WAKE_LOCK);
+       _mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+   }
 
   class MediaSessionCompatCallback extends MediaSessionCompat.Callback {
-
       @Override
       public void onPlay() {
           super.onPlay();
@@ -159,5 +159,10 @@ public class MPRemoteCommandCenterModule extends ReactContextBaseJavaModule {
 
   }
 
-
+  private void sendEvent(String eventName) {
+     Log.d(TAG, " **** ***** ==> " + eventName);
+     _reactContext
+        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+        .emit("RNMPRemoteCommandCenterEvent", eventName);
+    }
 }
